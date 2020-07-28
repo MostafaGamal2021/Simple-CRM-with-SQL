fa539cd62edc774a03a86348c67c3e81278772a8
Mostafa2021 <sofylove1990@gmail.com>
2020-07-28 10:07:07 +0200

    create a database and a table

--- CRM with SQL.py
+++ CRM with SQL.py
@@ -11,8 +11,25 @@ mydb = mysql.connector.connect(
         host = "localhost",
         user = "root",
         passwd = "professor2020",
+        database = "mga",
                                 )
 
-print(mydb)
+my_cursor = mydb.cursor()
+
+#my_cursor.execute("CREATE DATABASE mga")
+#print(mydb)
+
+#my_cursor.execute("SHOW DATABASES")
+
+#for i in my_cursor:
+    #print(i)
+
+#my_cursor.execute("CREATE TABLE customers (first_name VARCHAR(255),last_name VARCHAR(255),zipcode INT(10) ,price_paid DECIMAL(10, 2),user_id INT AUTO_INCREMENT PRIMARY KEY)")
+
+my_cursor.execute("SELECT * FROM customers")
+#print(my_cursor.description)
+for thing in my_cursor.description:
+    print(thing)
+
 
 root.mainloop()
